f4cc1ec2e264d6921cc5aed228bd835982ed726a
crgarcia8 <crgarcia8@gmail.com>
2021-04-29 18:44:34 -0500

    Add unit Test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,43 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './Redux/store';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+let globalUnmount;
+
+beforeEach(() => {
+  const { result, unmount } = render(<Provider store={store}>
+    <App />
+  </Provider>);
+  globalUnmount = unmount;
+})
+afterEach(() => {
+  globalUnmount();
+  globalUnmount = null;
+})
+
+test('Show text "Count:"', () => {
+  const countElement = screen.getByText(/count:/i);
+  expect(countElement).toBeInTheDocument();
+});
+test('Show count initial in 0', () => {
+  const countElement = screen.getByText(/count: 0/i);
+  expect(countElement).toBeInTheDocument();
+});
+
+test('Show count 1 when click in "Adicionar Valor"', () => {
+  expect(screen.getByText(/count: 0/i)).toBeInTheDocument();
+  const linkElement = screen.getByText(/adicionar valor/i);
+  fireEvent.click(linkElement);
+  expect(screen.getByText(/count: 1/i)).toBeInTheDocument();
+});
+
+test('Show count 5 when 5 clicks in "Adicionar Valor"', () => {
+  expect(screen.getByText(/count: 1/i)).toBeInTheDocument();
+  const linkElement = screen.getByText(/adicionar valor/i);
+  fireEvent.click(linkElement);
+  fireEvent.click(linkElement);
+  fireEvent.click(linkElement);
+  fireEvent.click(linkElement);
+  expect(screen.getByText(/count: 5/i)).toBeInTheDocument();
 });
